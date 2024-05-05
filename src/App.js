baee98fbc26b97e966e7ceb170b68a03c6170c39
deimos715 <deimos715@gmail.com>
2024-05-05 12:44:49 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from "react";
+import image from "./images/webpack.png";
+import "./style/style.scss";
+
+
+export const App = () => {
+    return (
+        <Fragment>
+            <div className="head">
+                <h1>Webpack</h1>
+            </div>
+
+            <div className="body">
+                <img src = {image}/>
+            </div>
+
+            <div className="footer">
+                Footer
+            </div>
+        </Fragment>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+require("@babel/polyfill");
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: "production", //development, production
+
+    entry: {
+        main: ["@babel/polyfill", "./src/index.js"],
+    },
+    
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+
+    devServer: {
+        port: 3000,
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src/index.html"),
+            inject: 'body',
+        }),
+
+        new CleanWebpackPlugin(),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                        ]
+                    }
+                }
+            },
+
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+
+            {
+                test: /\.less$/i,
+                use: ["style-loader", "css-loader", "less-loader"],
+            },
+
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                    }
+                ]
+            }
+        ]
+    }
+}
